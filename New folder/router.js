71c0be6206ea1c5e679378f9b6ae7ba1a3428b09
chirag2447 <chpanchal2447@gmail.com>
2023-08-02 19:23:18 +0530

    aaa

---
+++ New folder/router.js
@@ -0,0 +1,36 @@
+router.js
+const router = require("express").Router()
+
+
+
+router.get("/",(req,res)=>{
+    res.send("/ url called")
+})
+
+router.get("/hello",(req,res)=>{
+    res.send("hello world")   
+})
+
+router.get("/h*",(req,res)=>{})
+router.get("/hello/*",(req,res)=>{})
+
+router.get('/:name/:id',(req,res)=>{
+    res.send(`id is ${req.params.id} name is ${req.papams.name}`)
+})
+
+router.get("/page1",(req,res,next)=>{
+    res.write("handler 1 called ")
+    next()
+},(req,res)=>{
+    res.write("handler 2 called")
+    res.end()
+})
+
+router.post("/hello",(req,res)=>{
+    res.send("hello world from post")   
+})
+
+router.get('*',(req,res)=>{})//this should be in the last becuase it matches all the routes
+
+
+module.exports = router
